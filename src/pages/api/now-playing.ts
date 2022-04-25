1b9aaf4d8238236a994bba35181feaaf625f217b
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-04-25 22:57:45 +0700

    feat(api): setup spotify now-playing api route

---
+++ src/pages/api/now-playing.ts
@@ -0,0 +1,35 @@
+import { getNowPlaying } from "lib/services/spotify/user/now-playing";
+import type { GetNowPlayingTransformed } from "lib/services/spotify/user/now-playing/types";
+
+export const get = async (_, request) => {
+  if (request.method !== "GET") {
+    return new Response(undefined, { status: 400 });
+  }
+
+  try {
+    const response = await getNowPlaying();
+
+    if (!response || !response.item) {
+      return new Response(JSON.stringify({ isPlaying: false }), {
+        status: 200,
+      });
+    }
+
+    const data: GetNowPlayingTransformed = {
+      isPlaying: response.is_playing,
+      trackTitle: response.item.name,
+      artist: response.item.album.artists.map(({ name }) => name).join(", "),
+      album: response.item.album.name,
+      albumArtUrl: response.item.album.images[0].url,
+      trackUrl: response.item.external_urls.spotify,
+    };
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+    });
+  } catch {
+    return new Response(JSON.stringify({ isPlaying: false }), {
+      status: 200,
+    });
+  }
+};
