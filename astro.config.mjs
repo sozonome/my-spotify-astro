23170fccd89a1ad78e4674cbeb2d9388bba16d65
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-04-30 16:57:40 +0700

    chore: update packages to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import vercel from "@astrojs/vercel";
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
