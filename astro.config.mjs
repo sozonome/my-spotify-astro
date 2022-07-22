4c2b5e7f36088ffecbfe4799e7514003a19eafb1
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-07-22 18:15:49 +0700

    chore: set vercel runtime to edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
