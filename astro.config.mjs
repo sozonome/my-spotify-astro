cdbf255cf31caf48d1aca63a8f9b999d86d5cb50
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-08-10 18:57:32 +0700

    chore: update to astro v1.0.0 stable release

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  output: "server",
   integrations: [react(), tailwind()],
 });
