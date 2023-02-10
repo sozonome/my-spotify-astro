e3f1cc7b316e6c1410d006abfdbacf3061d47225
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-02-10 12:06:22 +0700

    chore: update astro to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  output: "server",
+  output: 'server',
   integrations: [react(), tailwind()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="astro/client" />
+
 interface ImportMetaEnv {
   readonly SPOTIFY_CLIENT_ID: string;
   readonly SPOTIFY_CLIENT_SECRET: string;
